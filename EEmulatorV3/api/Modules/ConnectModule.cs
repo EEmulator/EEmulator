baa05908599a690a81042ff8e0fed55f3e9911a3
atillabyte <atillabyte@null.net>
2020-05-05 14:41:48 -0400

    add connect module

---
+++ EEmulatorV3/api/Modules/ConnectModule.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EEmulatorV3.Messages;
+using Nancy;
+using ProtoBuf;
+
+namespace EEmulatorV3.api.Modules
+{
+    public class ConnectModule : NancyModule
+    {
+        public ConnectModule()
+        {
+            this.Post("/api/10", ctx =>
+            {
+                var args = Serializer.Deserialize<ConnectArgs>(this.Request.Body);
+
+                return PlayerIO.CreateResponse("token", true, new ConnectOutput()
+                {
+                    Token = args.GameId + ":" + args.UserId,
+                    UserId = args.UserId,
+                    ShowBranding = true,
+                    GameFSRedirectMap = "",
+                    PartnerId = "",
+                    PlayerInsightState = new PlayerInsightState() { PlayersOnline = 1, Segments = new List<KeyValuePair>() }
+                });
+            });
+        }
+    }
+
+    [ProtoContract]
+    public class ConnectArgs
+    {
+        [ProtoMember(1)]
+        public string GameId { get; set; }
+
+        [ProtoMember(2)]
+        public string ConnectionId { get; set; }
+
+        [ProtoMember(3)]
+        public string UserId { get; set; }
+
+        [ProtoMember(4)]
+        public string Auth { get; set; }
+
+        [ProtoMember(5)]
+        public string PartnerId { get; set; }
+
+        [ProtoMember(6)]
+        public List<string> PlayerInsightSegments { get; set; }
+
+        [ProtoMember(7)]
+        public string ClientAPI { get; set; }
+
+        [ProtoMember(8)]
+        public List<KeyValuePair> ClientInfo { get; set; }
+    }
+
+    [ProtoContract]
+    public class ConnectOutput
+    {
+        [ProtoMember(1)]
+        public string Token { get; set; }
+
+        [ProtoMember(2)]
+        public string UserId { get; set; }
+
+        [ProtoMember(3)]
+        public bool ShowBranding { get; set; }
+
+        [ProtoMember(4)]
+        public string GameFSRedirectMap { get; set; }
+
+        [ProtoMember(5)]
+        public string PartnerId { get; set; }
+
+        [ProtoMember(6)]
+        public PlayerInsightState PlayerInsightState { get; set; }
+    }
+}
